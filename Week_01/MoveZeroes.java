f49827b7350efa5b39b3b3fd6b9f840a3d58bf6d
Lujianze <1318956704@qq.com@gitee.com>
2020-04-20 13:27:31 +0800

    修改错误

--- Week_01/MoveZeroes.java
+++ Week_01/MoveZeroes.java
@@ -1,14 +1,19 @@
+package org.geekbang.ljz.Week_01;
+
+import org.junit.Test;
+
 public class MoveZeroes {
-    public static void main(String[] args) {
+    @Test
+    public void test() {
         MoveZeroes zeroes = new MoveZeroes();
-        zeroes.moveZeroes(new int {
+        zeroes.moveZeroes(new int[] {
             0, 1, 0, 3, 12
         });
     }
 
     public void moveZeroes(int[] nums) {
         int temp = -1;
-        for (int i = 0, j = 0; i < nums.length - 1; i++) {
+        for (int i = 0, j = 0; i < nums.length; i++) {
             if ((temp = nums[i]) != 0) {
                 nums[i] = temp ^ nums[j] ^ (nums[j++] = temp);
             }
