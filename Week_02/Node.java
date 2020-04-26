a39f0d59d8eb3f10493953264822761b773b1d2b
Lujianze <1318956704@qq.com@gitee.com>
2020-04-27 01:50:52 +0800

    node.children 空指针问题

--- Week_02/Node.java
+++ Week_02/Node.java
@@ -1,5 +1,6 @@
 package org.geekbang.ljz.Week_02;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Node {
@@ -11,6 +12,7 @@ public class Node {
 
     public Node(int val) {
         this.val = val;
+        this.children=new LinkedList<>();
     }
 
     public Node(int val, List<Node> children) {
--- Week_02/Preorder.java
+++ Week_02/Preorder.java
@@ -2,6 +2,7 @@ package org.geekbang.ljz.Week_02;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,15 +12,31 @@ import java.util.List;
  */
 public class Preorder {
     @Test
-    public void testPreorder(){
-        preorder(null);
+    public void testPreorder() {
+        Node root = new Node(1, getNodes(new int[]{3, 2, 4}));
+        for (Node node : root.children) {
+            if (node.val == 3) {
+                node.children = getNodes(new int[]{5, 6});
+            }
+        }
+        List<Integer> list = preorder(root);
+        System.out.println(Arrays.toString(list.toArray()));
     }
+
+    public LinkedList<Node> getNodes(int[] values) {
+        LinkedList<Node> nodes = new LinkedList<>();
+        for (int value : values) {
+            nodes.add(new Node(value));
+        }
+        return nodes;
+    }
+
     public List<Integer> preorder(Node root) {
         LinkedList<Node> input = new LinkedList<>();
         LinkedList<Integer> output = new LinkedList<>();
         if (root == null) return output;
         input.add(root);
-        while (!input.isEmpty()){
+        while (!input.isEmpty()) {
             Node node = input.pollLast();
             output.add(node.val);
             Collections.reverse(node.children);
